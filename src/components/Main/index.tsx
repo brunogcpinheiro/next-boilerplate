e4648a3cc8a628534da85237d68ff568663d4ee2
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2021-07-15 16:02:52 -0300

    boilerplate 1.0

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,21 @@
+import * as S from './styles'
+
+const Main = ({
+  title = 'React Avançado Boilerplate',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React avançado escrito ao lado"
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustation
+      src="img/hero-illustration.svg"
+      alt="desenvolvedor de frente para a tela de código"
+    />
+  </S.Wrapper>
+)
+
+export default Main
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,5 @@
-type Props = {
-  title: string
+import Main from 'components/Main'
+
+export default function Home() {
+  return <Main />
 }
-
-const bla = 'vat'
-
-const Home = ({ title = 'React Avançado' }: Props) => {
-  return (
-    <div>
-      <h1>Hello {title}!</h1>
-    </div>
-  )
-}
-
-export default Home
